81a4b8e0b93e5cef32e645a93e9aff822852fdf9
jordanavi <jordanavilela01@gmail.com>
2018-10-15 22:37:12 -0300

    Criando endpoint Eventos e testando api REST

--- src/main/java/com/integrador/igrejasonline/repositories/ResponsavelRepository.java
+++ src/main/java/com/integrador/igrejasonline/repositories/ResponsavelRepository.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Repository;
 
 import com.integrador.igrejasonline.domain.Responsavel;
 
-@Repository
-public class ResponsavelRepository extends JpaRepository <Responsavel, Integer>{
+//@Repository
+//public class ResponsavelRepository extends JpaRepository <Responsavel, Integer>{
  
 	
-	Responsavel findByCpf(String cpf);
-}
+	//Responsavel findByCpf(String cpf);
+//}
--- src/main/java/com/integrador/igrejasonline/resourcers/EventoResource.java
+++ src/main/java/com/integrador/igrejasonline/resourcers/EventoResource.java
@@ -1,5 +1,15 @@
 package com.integrador.igrejasonline.resourcers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/eventos")
 public class EventoResource {
 
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest esta funcionando";
+	}
 }
