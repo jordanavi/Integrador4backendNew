34e5c9aab04fbe01ffc1f2150e82c64bebedf3be
jordanavi <jordanavilela01@gmail.com>
2018-10-15 23:15:51 -0300

    Testando a primeira classe de dominio e endpoint - Evento

--- src/main/java/com/integrador/igrejasonline/domain/Evento.java
+++ src/main/java/com/integrador/igrejasonline/domain/Evento.java
@@ -1,6 +1,9 @@
 package com.integrador.igrejasonline.domain;
 
-public class Evento {
+import java.io.Serializable;
+
+public class Evento implements Serializable {
+	private static final long serialVersionUID = 1L;
 	//tratar endereco
 	//tratar data
 	
@@ -72,4 +75,31 @@ public class Evento {
 	public void setPublico(String publico) {
 		this.publico = publico;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Evento other = (Evento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
--- src/main/java/com/integrador/igrejasonline/resourcers/EventoResource.java
+++ src/main/java/com/integrador/igrejasonline/resourcers/EventoResource.java
@@ -1,15 +1,28 @@
 package com.integrador.igrejasonline.resourcers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.integrador.igrejasonline.domain.Evento;
 
 @RestController
 @RequestMapping(value="/eventos")
 public class EventoResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Evento> listar() {
+		
+		Evento ev1 = new Evento(1,"Encontro de Jovens", "09112018","09112018","Tema: juventude e sociedade", "jovens");
+		Evento ev2 = new Evento(2,"Encontro das filhas de Jó", "10112018","10112018","Tema: mulheres de fé", "mães");
+		
+		List<Evento> lista = new ArrayList<>();
+		lista.add(ev1);
+		lista.add(ev2);
+		
+		return lista;
 	}
 }
