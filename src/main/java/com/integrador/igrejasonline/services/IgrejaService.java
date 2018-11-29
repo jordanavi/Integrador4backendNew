a714436a9a3f32e16ddfb1c8362ffd477f413bee
jordanavi <jordanavilela01@gmail.com>
2018-11-29 19:32:06 -0200

    Alteracoes na classe igreja

--- src/main/java/com/integrador/igrejasonline/services/IgrejaService.java
+++ src/main/java/com/integrador/igrejasonline/services/IgrejaService.java
@@ -3,10 +3,16 @@ package com.integrador.igrejasonline.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.integrador.igrejasonline.repositories.EnderecoRepository;
+import com.integrador.igrejasonline.repositories.IgrejaRepository;
+
 //Esta classe serve para regras de negocio que não foram contempladas na camada de dominio.
 @Service
 public class IgrejaService<S>{
+	
 	@Autowired
-	private IgrejaR
+	private IgrejaRepository repo;
 
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 }
