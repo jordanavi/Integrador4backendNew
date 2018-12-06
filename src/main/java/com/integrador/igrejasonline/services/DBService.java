03a91f71801f083fa32a55b81ff9fba46e3a942b
Bruno Mundim <brunomundim3232@gmail.com>
2018-12-06 20:09:25 -0200

    Adicionando Estados do DBService

--- src/main/java/com/integrador/igrejasonline/services/DBService.java
+++ src/main/java/com/integrador/igrejasonline/services/DBService.java
@@ -32,8 +32,34 @@ public class DBService {
 
 	public void instantiateTestDatabase() throws ParseException {
 
-		Estado estado1 = new Estado(null, "Minas Gerais");
-		Estado estado2 = new Estado(null, "São Paulo");
+		Estado estado1 = new Estado(null, "Acre");
+		Estado estado2 = new Estado(null, "Alagoas");
+		Estado estado3 = new Estado(null, "Amapá");
+		Estado estado4 = new Estado(null, "Amazonas");
+		Estado estado5 = new Estado(null, "Bahia");
+		Estado estado6 = new Estado(null, "Ceará");
+		Estado estado7 = new Estado(null, "Distrito Federal");
+		Estado estado8 = new Estado(null, "Espírito Santo");
+		Estado estado9 = new Estado(null, "Goiás");
+		Estado estado10 = new Estado(null, "Maranhão");
+		Estado estado11 = new Estado(null, "Mato Grosso");
+		Estado estado12 = new Estado(null, "Mato Grosso do Sul");
+		Estado estado13 = new Estado(null, "Minas Gerais");
+		Estado estado14 = new Estado(null, "Pará");
+		Estado estado15 = new Estado(null, "Paraíba");
+		Estado estado16 = new Estado(null, "Paraná");
+		Estado estado17 = new Estado(null, "Pernambuco");
+		Estado estado18 = new Estado(null, "Piauí");
+		Estado estado19 = new Estado(null, "Rio de Janeiro");
+		Estado estado20 = new Estado(null, "Rio Grande do Norte");
+		Estado estado21 = new Estado(null, "Rio Grande do Sul");
+		Estado estado22 = new Estado(null, "Rondônia");
+		Estado estado23 = new Estado(null, "Roraima");
+		Estado estado24 = new Estado(null, "Santa Catarina");
+		Estado estado25 = new Estado(null, "São Paulo");
+		Estado estado26 = new Estado(null, "Sergipe");
+		Estado estado27 = new Estado(null, "Tocantins");
+
 
 		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
 		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
@@ -41,7 +67,9 @@ public class DBService {
 
 		estado1.getCidades().addAll(Arrays.asList(cidade1));
 		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
-		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		estadoRepository.saveAll(Arrays.asList(estado1,  estado2,  estado3,  estado4,  estado5,  estado6,  estado7,  estado8,  estado9,  estado10,
+											   estado11, estado12, estado13, estado14, estado15, estado16, estado17, estado18, estado19, estado20,
+											   estado21, estado22, estado23, estado24, estado25, estado26, estado27));
 		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
 
 		Endereco ende1 = new Endereco(null, "Rua do cavalo", "13", "Casa 1", "São Bento", "122334", cidade1);
